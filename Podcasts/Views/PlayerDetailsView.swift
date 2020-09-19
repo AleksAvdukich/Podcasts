f39dd2bb9bc4b5b09d8160e69c34736ae7953cea
Aleksandr Avdukich <avdukichsanel@mail.ru>
2020-09-19 23:43:49 +0300

    Launching our Podcast Player

---
+++ Podcasts/Views/PlayerDetailsView.swift
@@ -0,0 +1,31 @@
+//
+//  PlayerDetailsView.swift
+//  Podcasts
+//
+//  Created by Aleksandr Avdukich on 19.09.2020.
+//  Copyright © 2020 Aleksandr Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class PlayerDetailsView: UIView {
+    
+    var episode: Episode! {
+        didSet {
+            titleLabel.text = episode.title
+            
+            guard let url = URL(string: episode.imageUrl ?? "") else { return }
+            episodeImageView.sd_setImage(with: url)
+        }
+    }
+    
+    @IBAction func handleDismiss(_ sender: Any) {
+        self.removeFromSuperview()
+    }
+    
+    @IBOutlet weak var episodeImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+}
